de1459067b993032866a171bcb8ac18a60091af9
Stephen Schwarz <steve@steveandkureen.com>
2025-03-27 19:57:37 -0700

    removed bad tests

--- internal/workers/send-connection_test.go
+++ internal/workers/send-connection_test.go
@@ -2,7 +2,6 @@ package worker
 
 import (
 	"testing"
-	"time"
 )
 
 func TestCreateConnection(t *testing.T) {
@@ -49,33 +48,55 @@ func TestCreatePortBlank(t *testing.T) {
 	}
 }
 
-func TestConnectionStartStop(t *testing.T) {
-	connection, err := CreateSendConnection("connection id", "127.0.0.1", "59868")
+// func TestConnectionStartStop(t *testing.T) {
+// 	listen, err := SetupListenConnection("listen id", "127.0.0.1", "5986")
+// 	if err != nil {
+// 		t.Fatalf("Error creating listen connection: " + err.Error())
+// 	}
+// 	listen.Connect()
+// 	connection, err := CreateSendConnection("connection id", "127.0.0.1", "5986")
+// 	if err != nil {
+// 		t.Fatalf("Error creating connection: " + err.Error())
+// 	}
+//
+// 	connection.Connect()
+//
+// 	if listen.connected == false {
+// 		t.Fatalf("Listen connection is not connected")
+// 	}
+// 	if connection.connected == false {
+// 		t.Fatalf("Send connection is not connected")
+// 	}
+//
+// 	time.Sleep(1 * time.Second)
+// 	connection.Disconnect()
+//
+// }
+
+//	func TestConnect(t *testing.T) {
+//		setupMockDialTimeout()
+//
+//		connection, err := CreateSendConnection("connection id", "127.0.0.1", "59868")
+//		if err != nil {
+//			t.Fatalf(err.Error())
+//		}
+//
+//		if connection != nil {
+//			t.Fatalf("Connection should be nil")
+//		}
+//
+//		connection.Connect()
+//
+//		if !connection.connected {
+//			t.Fatalf("Connection is not connected")
+//		}
+//
+// }
+func SetupListenConnection(id string, ip string, port string) (*ListenConnection, error) {
+	connection, err := CreateListenConnection(id, ip, port)
 	if err != nil {
-		t.Fatalf("Error creating connection: " + err.Error())
+		return nil, err
 	}
-
 	connection.Connect()
-	time.Sleep(1 * time.Second)
-	connection.Disconnect()
-}
-
-func TestConnect(t *testing.T) {
-	setupMockDialTimeout()
-
-	connection, err := CreateSendConnection("connection id", "127.0.0.1", "59868")
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	if connection != nil {
-		t.Fatalf("Connection should be nil")
-	}
-
-	connection.Connect()
-
-	if !connection.connected {
-		t.Fatalf("Connection is not connected")
-	}
-
+	return connection, nil
 }
